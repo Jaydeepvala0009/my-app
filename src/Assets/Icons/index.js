6b8fc8d6856f2030c4ec41b0c962ccb621d86134
My Name <thesame@email.com>
2024-02-13 18:32:47 +0530

    index file

---
+++ src/Assets/Icons/index.js
@@ -0,0 +1,29 @@
+import { IoIosHeart } from "react-icons/io";
+import { MdOutlineShoppingBag } from "react-icons/md";
+import { FaCircle } from "react-icons/fa6";
+import { CiStar } from "react-icons/ci";
+import { FaStar } from "react-icons/fa6";
+import { MdOutlineStarHalf } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
+import { MdPayment } from "react-icons/md";
+import { RiSecurePaymentFill } from "react-icons/ri";
+import { FaCreditCard } from "react-icons/fa";
+import { AiOutlineShopping } from "react-icons/ai";
+import { FaRegHeart } from "react-icons/fa";
+import { IoSearch } from "react-icons/io5";
+
+export {
+  IoIosHeart,
+  MdOutlineShoppingBag,
+  FaCircle,
+  CiStar,
+  FaStar,
+  MdOutlineStarHalf,
+  MdDelete,
+  MdPayment,
+  RiSecurePaymentFill,
+  FaCreditCard,
+  AiOutlineShopping,
+  FaRegHeart,
+  IoSearch,
+};
